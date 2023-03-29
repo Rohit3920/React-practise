94e49d0f253195571a524f8a406824d4840ba56c
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-03-28 22:08:50 -0700

    first Example for react and react component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import User from './User'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,7 +23,11 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"> Learn React</a>
-      </header>
+      </header> */}
+
+
+      <h1>rohit nittawadekar</h1>
+      <User />
     </div>
   );
 }
---
+++ src/User.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './User.css'
+
+function User(){
+    return(
+        <div>
+            <h1 className="UserH1">User component   :)</h1>
+        </div>
+    )
+}
+
+export default User;
