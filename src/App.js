c46e57597a44b185d53015e040da81c57d37fe57
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-04-12 08:19:29 -0700

    Create LOGIN Form with Design and Responsive From Using React Login.js and Login.css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import {Button} from "react-bootstrap"
-import React, { useState, useEffect } from 'react';
-import User1 from "./User1"
+import { Button } from "react-bootstrap"
+import React from 'react';
+import Login from './Component/Login';
 
 function App() {
-  const [data, setData] = useState(10);
-  const [count, setCount] = useState(1);
-
   return (
     <div className="App">
 
-      <User1 count={count} data={data} />
-
-      <Button variant = "warning" onClick={() => setData(data + 1)}>Update Data {data}</Button>
-      <Button onClick={() => setCount(count + 1)}>Update Count {count}</Button>
-      
-
+      <Login />
     </div>
   );
 }
---
+++ src/Component/Login.js
@@ -0,0 +1,44 @@
+import React, { useState, Fragment } from "react";
+import './Login.css';
+import { Button } from "react-bootstrap";
+
+export default function Login() {
+    const [user, setUser] = useState();
+    const [password, setPassword] = useState();
+    const [userErr, setUserErr] = useState(false);
+    const [passwordErr, setPasswordErr] = useState(false);
+
+    function loginHandler(e) {
+        e.preventDefault();
+    }
+
+    function userHandler(e) {
+        let item = e.target.value
+        item.length < 3 ? setUserErr(true) : setUserErr(false)
+    }
+
+    function passwordHandler(e) {
+        let item = e.target.value
+        item.length < 3 ? setPasswordErr(true) : setPasswordErr(false)
+    }
+
+    return (
+        <div className="LoginDiv">
+            <h1>LOGIN FORM</h1>
+
+            <form onSubmit={loginHandler}>
+                <input type="text" placeholder="Enter User Name" onChange={userHandler} /><br/>
+                {userErr ? <span>User Not Valid</span>: <span></span>}
+                <br /><br />
+
+                <input type="password" placeholder="Enter Password" onChange={passwordHandler} /><br/>
+                {passwordErr ? <span>Password Not Valid</span> : <span></span>}
+
+
+                <br /><br />
+
+                <Button type="submit">LogIn</Button>
+            </form>
+        </div>
+    )
+}
