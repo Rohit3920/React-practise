5a2262cae808e74fd1a41075da1fcb452b29eef5
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-04-17 20:22:24 -0700

    Same changes props Component and its styling

--- src/Component/PropFun.js
+++ src/Component/PropFun.js
@@ -1,13 +1,15 @@
-import "./style.css"
 import {Fragment} from 'react' 
+import "./style.css"
 function PropFun(props){
     console.warn(props)
     return (
-        <Fragment className="main">
+        <Fragment>
+        <div className="main">
             <p>props with Function Component</p>
             <h1>{props.name}</h1>
             <h1>{props.email}</h1>
             <h1>{props.other.address}</h1>
+            </div>
         </Fragment>
     )
 }
--- src/Component/PropsClass.js
+++ src/Component/PropsClass.js
@@ -5,12 +5,14 @@ class PropsClass extends Component{
     render(){
         console.warn(this.props)
         return(
-            <Fragment className="main">
+            <Fragment>
+                <div className="main">
                 <p>Props with Class Component</p>
                 <h1>Student : {this.props.name}</h1>
+                </div>
             </Fragment>
         )
     }
 }
 
-export default PropsClass
+export default PropsClass;
