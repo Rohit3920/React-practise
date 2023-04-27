fb620b531b58166641e54b65a55e543116a6a9a8
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-04-27 21:45:59 +0530

    useEffect Hook with specific state and data ; and Props specific

--- src/Component/HookUseEffect.js
+++ src/Component/HookUseEffect.js
@@ -5,19 +5,30 @@ import { Button } from "react-bootstrap";
 function HookUseEffect(){
 
     const [count, setCount] = useState(0);
+    const [data, setData] = useState(0);
+
 
     useEffect(() => {
         setTimeout(() => {
-            console.log(" Use Effect CALLED", count)
+            console.log(" Use Effect CALLED count", count)
         }, count*1000);
-    })
+    }, [count])
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log(" Use Effect CALLED data", data)
+        }, data*1000);
+    }, [data])
 
     return(
         <div>
-            <h1>React Hook : UseEffect {count}</h1>
+            <h1>React Hook : UseEffect </h1>
+            <h2> count : {count} <br /> Data: {data} </h2>
             <p> useEffect are React HOOK and setTimeOut function called</p>
 
-        <Button onClick={()=> { setCount(count+1)}}>Incre-Counter</Button>
+        <Button onClick={()=> { setCount(count+1)}}>Incre-Counter</Button><br/><br/>
+        <Button onClick={()=> { setData(data+1)}}>Incre-Data</Button>
+
 
         </div>
     )
