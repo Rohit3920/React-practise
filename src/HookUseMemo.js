ad0803474e8ff3f12ef895a405ca1bea19229d6a
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-04-30 15:29:46 +0530

    same mistake from HookUseMemo.js files are currect then successfully work code

--- src/HookUseMemo.js
+++ src/HookUseMemo.js
@@ -1,7 +1,6 @@
 import { useMemo, useState } from "react";
 import { Button } from "react-bootstrap";
 
-
 function HookUseMemo(){
 
     const [count, setCount] = useState(1);
@@ -10,15 +9,12 @@ function HookUseMemo(){
     const multiCountMemo = useMemo(function multiCount(){
         console.log("MultiCount", count)
        return count + 2;
-    })
-    console.log(multiCountMemo)
-
-
-
+    }, [count])
 
     return (
         <div>
             <h1>useMemo Hook in React JS</h1>
+            <p>useMemo are mainly work as a special function for perticular state as a define</p>
             <h4>Count: {count}</h4>
             <h4>Item: {item}</h4>
 
@@ -26,8 +22,6 @@ function HookUseMemo(){
             <Button onClick={()=>setItem(item * 2)}>item Inc</Button>
         </div>
     )
-
-
 }
 
 export default HookUseMemo;
